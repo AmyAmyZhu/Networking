99ddb3befff69d76ead69415e454a8037b335396
AmyAmyZhu <zhuzefang12@gmail.com>
2016-10-18 00:04:29 -0400

    change

--- A1/src/client.java
+++ A1/src/client.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  * Created by amyzhu on 16/10/16.
  */
@@ -8,7 +16,7 @@ public class client {
         String req_code;
 
         // Error: wrong number of inputs
-        if(argv.length != 4){
+        if(argv.length != 4) {
             System.out.println("Error: Wrong number of inputs.");
             System.out.println("Number of parameters: 4");
             System.out.println("Parameter:");
@@ -28,6 +36,35 @@ public class client {
         server_address = argv[0];
         n_port = Integer.parseInt(argv[1]);
         req_code = argv[2];
+
+        Socket TCP_socket = new Socket(server_address, n_port);
+        DataOutputStream outputStream = new DataOutputStream(TCP_socket.getOutputStream());
+        outputStream.writeBytes(req_code + "\n");
+
+        BufferedReader from_server = new BufferedReader(new InputStreamReader(TCP_socket.getInputStream()));
+        int random_port = Integer.parseInt(from_server.readLine());
+        System.out.println("Random Port: " + random_port);
+        TCP_socket.close();
+
+        DatagramSocket UDP_socket = new DatagramSocket();
+        sendMessage(random_port, argv, server_address, UDP_socket);
+        UDP_socket.close();
+
+    }
+
+    private static void sendMessage(int random_port, String argv[], String server_address, DatagramSocket UDP_socket) throws Exception {
+        byte[] receive_message = new byte[1024];
+        byte[] send_message = new byte[1024];
+
+        String reverse_string = argv[3];
+        send_message = reverse_string.getBytes();
+        InetAddress ip = InetAddress.getByName(server_address);
+        DatagramPacket send_packet = new DatagramPacket(send_message, send_message.length, ip, random_port);
+        UDP_socket.send(send_packet);
+        DatagramPacket receive_packet = new DatagramPacket(receive_message, receive_message.length);
+        UDP_socket.receive(receive_packet);
+        String changed_string = new String(receive_packet.getData());
+        System.out.println("Reversed String: " + changed_string);
 
     }
 }
--- A1/src/server.java
+++ A1/src/server.java
@@ -1,4 +1,7 @@
-import java.net.ServerSocket;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.*;
 
 /**
  * Created by amyzhu on 16/10/16.
@@ -9,7 +12,7 @@ public class server {
         int req_code;
 
         // Error: wrong number of inputs
-        if(argv.length != 1){
+        if(argv.length != 1) {
             System.out.println("Error: Wrong number of inputs.");
             System.out.println("Number of parameters: 1");
             System.out.println("Parameter:");
@@ -17,7 +20,7 @@ public class server {
             System.exit(0);
         } try {
             req_code = Integer.parseInt(argv[0]);
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error: req_code should be an integer");
             System.exit(0);
         }
@@ -26,9 +29,52 @@ public class server {
         req_code = Integer.parseInt(argv[0]);
 
         // Create a TCP socket
-        ServerSocket TCPSocket = new ServerSocket(0);
+        ServerSocket TCP_socket = new ServerSocket(0);
         // Get port from TCP socket
-        n_port = TCPSocket.getLocalPort();
+        n_port = TCP_socket.getLocalPort();
         System.out.println("SERVER_PORT=" + n_port);
+
+        while (true) {
+            // Connect client socket and TCP socket
+            Socket client_socket = TCP_socket.accept();
+            // Compare client received code and request code
+            compareCode(req_code, client_socket);
+
+            DataOutputStream outputStream = new DataOutputStream(client_socket.getOutputStream());
+            DatagramSocket UDP_socket = new DatagramSocket(0);
+            int random_port = UDP_socket.getLocalPort();
+            outputStream.writeBytes(random_port + "\n");
+            reverseString(UDP_socket);
+            UDP_socket.close();
+        }
+    }
+
+    private static void compareCode(int req_code, Socket client_socket) throws Exception {
+        // Get code client received from a stream from client
+        BufferedReader client_read = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
+        int client_code = Integer.parseInt(client_read.readLine());
+
+        // Error: code client received not equal to req_code
+        if(req_code != client_code) {
+            client_socket.close();
+            System.out.println("Error: code client received not euqal to req_code");
+            System.exit(0);
+        }
+    }
+
+    private static void reverseString(DatagramSocket UDP_socket) throws Exception {
+        byte[] receive_message = new byte[1024];
+        byte[] send_message = new byte[1024];
+
+        DatagramPacket receive_packet = new DatagramPacket(receive_message, receive_message.length);
+        UDP_socket.receive(receive_packet);
+        String client_string = new String(receive_packet.getData());
+        InetAddress IPA_address = receive_packet.getAddress();
+        int sb_port = receive_packet.getPort();
+        String reverse_string = new StringBuilder(client_string).reverse().toString();
+        send_message = reverse_string.getBytes();
+        DatagramPacket send_packet = new DatagramPacket(send_message, send_message.length, IPA_address, sb_port);
+        UDP_socket.send(send_packet);
+
     }
 }
